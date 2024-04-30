77467d4a7b29212ccacf6d07193222115334736c
Franklin Obiukwu <obiukwuchibuisi@gmail.com>
2024-04-30 22:40:50 +0100

    Function that accept three arguments and  return an array

---
+++ 0x01-ES6_promise/6-final-user.js
@@ -0,0 +1,26 @@
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
+
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  const user = signUpUser(firstName, lastName);
+  const photo = uploadPhoto(fileName);
+
+  return Promise.allSettled([user, photo])
+    .then((responses) => {
+      const arr = responses.map((response) => {
+        if (response.status === 'fulfilled') {
+          return {
+            status: response.status,
+            value: response.value
+          };
+        }
+        else if(response.status === 'rejected') {
+          return {
+            status: response.status,
+            reason: response.reason
+          };
+        }
+      });
+      return arr;
+    });
+}
---
+++ 0x01-ES6_promise/6-main.js
@@ -0,0 +1,3 @@
+import handleProfileSignup from "./6-final-user.js"
+const x = handleProfileSignup("Franklin", "Obiukwu", "myhead.jpg")
+console.log("This is the return value of handleProfileSignup", x)
