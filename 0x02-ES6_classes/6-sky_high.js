76971696c9bfb98e3b6f7084bdbbbbaaf1afbc29
Franklin Obiukwu <obiukwuchibuisi@gmail.com>
2024-05-09 02:43:12 +0100

    Implemented class `SkyHighBuilding` that extends from `Building`

--- 0x02-ES6_classes/6-sky_high.js
+++ 0x02-ES6_classes/6-sky_high.js
@@ -2,28 +2,19 @@ import Building from './5-building';
 
 export default class SkyHighBuilding extends Building {
   constructor(sqft, floors) {
-    super();
-    Building._sqft = sqft;
+    super(sqft);
     this._floors = floors;
   }
 
   get sqft() {
-    return Building._sqft;
-  }
-
-  set sqft(value) {
-    Building._sqft = value;
+    return this._sqft;
   }
 
   get floors() {
     return this._floors;
   }
 
-  set floors(value) {
-    this._floors = value;
-  }
-
   evacuationWarningMessage() {
-    return 'Evacuate slowly the NUMBER_OF_FLOORS floors';
+    return `Evacuate slowly the ${this._floors} floors`;
   }
 }
