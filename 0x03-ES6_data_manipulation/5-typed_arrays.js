19f80bba090351bbd5d96922a2e1118d4bf26c72
Franklin Obiukwu <obiukwuchibuisi@gmail.com>
2024-05-18 03:06:36 +0100

    Fixed ESLint issues

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,13 +1,12 @@
 const createInt8TypedArray = (length, position, value) => {
+  if (position > length - 1 || position < 0) {
+    throw new Error('Position outside range');
+  }
+  const arrBuff = new ArrayBuffer(length);
+  const dataView = new DataView(arrBuff);
+  dataView.setInt8(position, value);
 
-    if (position > length - 1 || position < 0) {
-        throw new Error("Position outside range")
-    }
-    const arrBuff = new ArrayBuffer(length)
-    const dataView = new DataView(arrBuff)
-    dataView.setInt8(position, value)
+  return dataView;
+};
 
-    return dataView
-}
-
-export default createInt8TypedArray
+export default createInt8TypedArray;
