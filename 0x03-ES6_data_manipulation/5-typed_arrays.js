caedd04315897e7479163769f45d5e2aa0aad347
Franklin Obiukwu <obiukwuchibuisi@gmail.com>
2024-05-18 03:05:11 +0100

    Typed Array

---
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -0,0 +1,13 @@
+const createInt8TypedArray = (length, position, value) => {
+
+    if (position > length - 1 || position < 0) {
+        throw new Error("Position outside range")
+    }
+    const arrBuff = new ArrayBuffer(length)
+    const dataView = new DataView(arrBuff)
+    dataView.setInt8(position, value)
+
+    return dataView
+}
+
+export default createInt8TypedArray
