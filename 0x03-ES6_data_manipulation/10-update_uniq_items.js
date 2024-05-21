e68afad08e82dcb4376e9efac42dbaeb8aa78e0d
Franklin Obiukwu <obiukwuchibuisi@gmail.com>
2024-05-21 17:53:23 +0100

    Function that updates map

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -1,10 +1,13 @@
 const updateUniqueItems = (map) => {
-    if (!(map instanceof Map)) {
-        return
-    }
-    const array = Array.from(map)
-    map.entries().forEach((item) => {
-        if (item[1] === 1) item[1] = 100
-    })
-}
-export default updateUniqueItems
+  if (!(map instanceof Map)) {
+    return;
+  }
+  const array = Array.from(map);
+  const newArray = array.map((item) => {
+    if (item[1] === 1) item[1] = 100;
+    return item;
+  });
+
+  return new Map(newArray);
+};
+export default updateUniqueItems;
