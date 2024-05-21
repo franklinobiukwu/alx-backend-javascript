08a15d110328649cc6e39952bdfb2fa9c21ac829
Franklin Obiukwu <obiukwuchibuisi@gmail.com>
2024-05-21 18:06:08 +0100

    Fixed ESLint issues

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -1,11 +1,13 @@
+// eslint-disable-next-line consistent-return
 const updateUniqueItems = (map) => {
   if (!(map instanceof Map)) {
     return;
   }
   const array = Array.from(map);
   const newArray = array.map((item) => {
-    if (item[1] === 1) item[1] = 100;
-    return item;
+    const itemCopy = [...item];
+    if (item[1] === 1) itemCopy[1] = 100;
+    return itemCopy;
   });
 
   return new Map(newArray);
