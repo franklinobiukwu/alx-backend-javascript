4489bf8925a543630d69b8fdbda15473b57da6b9
Franklin Obiukwu <obiukwuchibuisi@gmail.com>
2024-05-21 16:37:16 +0100

    function named groceriesList that returns a map of groceries with the following items (name, quantity):

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -2,6 +2,7 @@ const cleanSet = (set, startString) => {
   // function that returns a string of all the set values that start with a specific string
   const arr = [...set];
   // filter array element
-  return arr.filter((item) => item.startsWith(startString));
+  const filteredString =  arr.filter((item) => item.startsWith(startString));
+    filteredString.forEach((item) => item.split())
 };
 export default cleanSet;
