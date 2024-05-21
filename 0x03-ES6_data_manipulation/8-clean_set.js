28b501bf5fa6183a43c93530d4c5946e16ee6e29
Franklin Obiukwu <obiukwuchibuisi@gmail.com>
2024-05-21 18:00:19 +0100

    Fixed code undifined error

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -3,7 +3,7 @@ const cleanSet = (set, startString) => {
   // function that returns a string of all the set values that start with a specific string
   const arr = [...set];
   // filter array element
-  const filteredString = arr.filter((item) => item.startsWith(startString));
+  const filteredString = arr.filter((item) => item && item.startsWith(startString));
   const trimmedString = filteredString.map((item) => item.slice(startString.length));
   return trimmedString.join('-');
 };
