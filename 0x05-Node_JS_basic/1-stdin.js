c3996dff83917608733bff5d92ee4897582569e2
Franklin Obiukwu <obiukwuchibuisi@gmail.com>
2024-11-01 06:10:41 +0100

    Changed console.log to process.stdout.write

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,11 +1,11 @@
 // Display Message
-console.log('Welcome to Holberton School, what is your name?')
+process.stdout.write('Welcome to Holberton School, what is your name?\n')
 
 // Accept input from stdin
 process.stdin.on('readable', () => {
     const name = process.stdin.read()
     if (name !== null){
-        console.log(`Your name is: ${name}`)
+        process.stdout.write(`Your name is: ${name}`)
     }
 })
 
